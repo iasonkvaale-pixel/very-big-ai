ff8e25b660542553ba9c42ece074edb5f9c19fe4
iasonkvaale-pixel <iason.kvaale@gmail.com>
2026-04-15 19:30:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function sendMessage() {
     input.value = "";
 
     try {
-        const response = await fetch("http://localhost:3000/chat", {
+        const response = await fetch("https://din-server.onrender.com/chat", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
